4c25913f55a2b5936dce220f086df0d4480cac49
jiminpark23 <jimin4124.22@gmail.com>
2023-04-08 23:17:57 +0900

    로그인, 회원가입 기능 (Auth, RealtimeDatabase)

--- app/src/main/java/com/example/lostbox/LoginActivity.java
+++ app/src/main/java/com/example/lostbox/LoginActivity.java
@@ -1,24 +1,29 @@
 package com.example.lostbox;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import static android.widget.Toast.*;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
-    //AppCompatButton btn_log;
-    //TextView sign;
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    private EditText mEtId, mEtPwd;         // 로그인 입력필드
 
 
     @Override
@@ -26,8 +31,42 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        AppCompatButton btn_log = findViewById(R.id.button);
-        TextView sign = findViewById(R.id.textView2);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("LostBox");
+
+        mEtId = findViewById(R.id.et_login_id);
+        mEtPwd = findViewById(R.id.et_login_pwd);
+
+
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 로그인 요청
+                String strId = mEtId.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strId, strPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();   // 현재 액티비티 파괴
+                        } else {
+                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+
+                AppCompatButton btn_log = findViewById(R.id.btn_login);
+        TextView sign = findViewById(R.id.tv_signup);
 
         btn_log.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/lostbox/UserAccount.java
@@ -0,0 +1,63 @@
+package com.example.lostbox;
+
+/*
+ * 사용자 계정 정보 모델 클래스
+ */
+public class UserAccount {
+    private String idToken;    // Firebase Uid (고유 토큰정보)
+    private String emailId;    // 이메일 아이디
+    private String password;   // 비밀번호
+    private String name;       // 이름
+    private String id;         // 아이디
+    private String phone;      // 전화번호
+
+    public UserAccount() { }   // 빈 생성자
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
