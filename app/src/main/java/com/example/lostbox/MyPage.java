c56ba1293aeb2bf19eafda5a5c73379d6b940441
jiminpark23 <jimin4124.22@gmail.com>
2023-04-29 02:22:10 +0900

    홈화면 fragment

---
+++ app/src/main/java/com/example/lostbox/MyPage.java
@@ -0,0 +1,23 @@
+package com.example.lostbox;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class MyPage extends Fragment {
+
+    public MyPage() {
+
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mypage, container, false);
+    }
+}
