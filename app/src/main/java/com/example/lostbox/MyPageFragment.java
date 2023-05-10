6062bdf9fb5d6954b3541c8a12428b9f01e10216
jiminpark23 <jimin4124.22@gmail.com>
2023-05-10 12:05:05 +0900

    로그아웃 기능

--- app/src/main/java/com/example/lostbox/MyPageFragment.java
+++ app/src/main/java/com/example/lostbox/MyPageFragment.java
@@ -1,15 +1,24 @@
 package com.example.lostbox;
 
-//import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import androidx.annotation.Nullable;
 
-public class MyPageFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MyPageFragment extends Fragment implements View.OnClickListener {
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
 
     public MyPageFragment() {
 
@@ -19,6 +28,31 @@ public class MyPageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mypage, container, false);
+        View view = inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("LostBox");
+
+        TextView signout = view.findViewById(R.id.tv_signOut);
+        signout.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tv_signOut) {
+            signOut();
+        }
+    }
+
+    private void signOut() {
+        FirebaseAuth.getInstance().signOut();
+
+        // 로그아웃 후 동작 (로그인 페이지로 전환)
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish(); // 이전 화면을 종료하고 돌아가는 경우
+
     }
 }
