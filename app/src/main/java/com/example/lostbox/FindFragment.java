30694f452511ad827071527c3489127dee48ad77
윤해빈 <ywy2001@naver.com>
2023-05-17 12:40:49 +0900

    FoundAdapter 연결

--- app/src/main/java/com/example/lostbox/FindFragment.java
+++ app/src/main/java/com/example/lostbox/FindFragment.java
@@ -7,13 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
---
+++ app/src/main/java/com/example/lostbox/FoundItem.java
@@ -0,0 +1,54 @@
+package com.example.lostbox;
+
+public class FoundItem {
+    private String title;
+    private String place;
+    private String date;
+    private String content;
+    private int img;
+
+    public String getTitle(){
+        return title;
+
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getPlace(){
+        return place;
+
+    }
+
+    public void setPlace(String place){
+        this.place = place;
+    }
+
+    public String getDate(){
+        return date;
+
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+
+    public String getContent(){
+        return content;
+
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public int getImg(){
+        return img;
+
+    }
+
+    public void setImg(int img){
+        this.img = img;
+    }
+}
