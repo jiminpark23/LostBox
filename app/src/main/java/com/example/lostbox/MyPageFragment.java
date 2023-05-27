e687c5e2968b9758d44e02ddd4e32964112447f2
jiminpark23 <jimin4124.22@gmail.com>
2023-05-28 01:33:57 +0900

    마이페이지 유저네임

--- app/src/main/java/com/example/lostbox/MyPageFragment.java
+++ app/src/main/java/com/example/lostbox/MyPageFragment.java
@@ -13,12 +13,16 @@ import androidx.fragment.app.Fragment;
 import androidx.annotation.Nullable;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MyPageFragment extends Fragment implements View.OnClickListener {
     private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
     private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    private TextView nickname;  // 사용자 닉네임을 보여주기 위함
 
     public MyPageFragment() {
 
@@ -33,10 +37,39 @@ public class MyPageFragment extends Fragment implements View.OnClickListener {
         mFirebaseAuth = FirebaseAuth.getInstance();
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("LostBox");
 
-        TextView signout = view.findViewById(R.id.tv_signOut);
+        nickname = view.findViewById((R.id.tv_nickname));  // 닉네임 표시를 위함
+        TextView signout = view.findViewById(R.id.tv_signOut);  // 로그아웃 기능을 위함
+
+        fetchUserName();    // 사용자 이름을 가져옴
         signout.setOnClickListener(this);
 
         return view;
+    }
+
+    private void fetchUserName() {
+        // 현재 사용자의 UID를 가져옴
+        String uid = mFirebaseAuth.getCurrentUser().getUid();
+
+        // UserAccount 레퍼런스에서 현재 사용자의 이름을 가져옴
+        DatabaseReference userRef = mDatabaseRef.child("UserAccount").child(uid);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // 사용자 이름을 가져와서 TextView에 설정
+                if (snapshot.exists()) {
+                    UserAccount user = snapshot.getValue(UserAccount.class);
+                    if (user != null) {
+                        String name = user.getName();
+                        nickname.setText(name);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // 에러 처리
+            }
+        });
     }
 
     @Override
